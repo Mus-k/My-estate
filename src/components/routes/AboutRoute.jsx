3d81d485b6f9b83ef4e4f162fe6489ad1fa5e5ef
Musa Kamara <kamaramusa56@gmail.com>
2023-05-14 01:31:58 +0300

    done

--- src/components/routes/AboutRoute.jsx
+++ src/components/routes/AboutRoute.jsx
@@ -1,43 +1,46 @@
 import React from 'react'
+import './About.css'
 import { Link } from 'react-router-dom'
 import {default as aboutImg } from '../../assets/images/aboutImg.jpg'
+import { Overview } from '../overview/Overview'
+import { NewLetter } from '../newsletter/NewLetter'
+import { Footer } from '../footer/Footer'
 export const AboutRoute = () => {
   return (
-    <section className=" h-[100vh] w-[100%] bg-[#f6f8fa]">
-      <div className="flex justify-center items-center bg-slate-500 w-[100%] pt-14 h-[120px] md:h-[300px]">
-        <div className="flex gap-3 justify-center md:pt-10 items-center ">
-          <h1 className="text-xl text-black font-sans font-semibold md:text-6xl ">
+    <section className="about">
+      <div className="ab-container">
+        <div className="ab-inner">
+          <h1 className="ab-title">
             Home
           </h1>
           <i
-            class="fa-solid fa-arrows-turn-right
-          text-white text-center pt-2 text-2xl"
+            class="fa-solid fa-arrows-turn-right"
           ></i>
-          <i class="fa-sharp fa-solid fa-slash-forward text-white text-center pt-2 text-2xl"></i>
-          <h3 className="text-[#6663e9] text-lg md:text-5xl">About</h3>
+        
+          <h3 className="ab-subTitle">About</h3>
         </div>
       </div>
-      <section className="flex w-[100%] flex-col justify-center items-center md:flex-row  gap-4 md:gap-8 pt-3 pb-6 px-20 md:px-32">
-        <div className="flex-1">
+      <section className="ab-section">
+        <div className="ab-img">
           <img
             src={aboutImg}
             alt="about us"
-            className="md:max-w-[400px] lg:max-w-[600px] md:h-[350px]"
+           width='100%'
           />
         </div>
-        <div className="flex-1 w-[100%] flex flex-col bg-black  text-white gap-3 md:gap-4 justify-center items-start">
-          <h1 className="text-[25px] md:text-4xl font-sans font-semibold">
+        <div className="ab-left">
+          <h1 className="left-title">
             About us
           </h1>
 
-          <p className="md:hidden">
+          {/* <p className="md:hidden">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. Duis aute irure dolor in est
             laborum
-          </p>
-          <p className="hidden md:flex  w-[100%] text-sm lg:text-xl">
+          </p> */}
+          <p className="left-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -47,13 +50,15 @@ export const AboutRoute = () => {
             culpa qui officia deserunt mollit anim id est laborum
           </p>
 
-          <button className="bg-[#6663e9]  rounded-3xl py-2 md:py-3 px-6 hover:bg-white transition-all duration-500">
+          <button className="golden-btn">
             <Link to="/contact"
-            className='ab-link'>contact us</Link>
+            className='g-link'>contact us</Link>
           </button>
         </div>
       </section>
-    
+    <Overview/>
+    <NewLetter/>
+    <Footer/>
     </section>
   )
 }
