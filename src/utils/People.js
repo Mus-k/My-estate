353bbc83612b9992c45103557c306984ad977ff5
Musa Kamara <kamaramusa56@gmail.com>
2023-05-15 13:16:47 +0300

    done

--- src/utils/People.js
+++ src/utils/People.js
@@ -1,40 +1,29 @@
 import { default as img1 } from "../assets/images/profile-4.jpg";
 import { default as img2 } from "../assets/images/profile-3.jpg";
-import { default as img3 } from "../assets/images/profile-3.jpg";
-import { default as img4 } from "../assets/images/tesimonial.jpg";
-
+import { default as img3 } from "../assets/images/tesimonial.jpg";
 
 const reviews = [
   {
     id: 1,
-    fname: "Ahmet",
-    job:"Web developer",
-    image:img1,
+    fname: "Susan",
+    job: "Web developer",
+    image: img1,
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum  ",
   },
   {
     id: 2,
-    fname: "susan smith",
-    job:"FullStack developer",
-    image:img2,
+    fname: "Van smith",
+    job: "FullStack developer",
+    image: img2,
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum ",
   },
   {
     id: 3,
     fname: "Peter johnson",
-    job:"Businessman",
-    image:img3,
+    job: "Businessman",
+    image: img3,
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum ",
   },
-  {
-    id: 4,
-    fname: "Veronica",
-    job:"Dentish",
-    image: img4,
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum  ",
-  },
-
 ];
-
 
 export default reviews;
